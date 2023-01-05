2e1de619343e0604163dca63d4b4a334796a114f
汤纪阔 <tangjikuo@cmiot.chinamobile.com>
2023-01-05 17:59:33 +0800

    添加对每天的练习文件

---
+++ 01_Day_Introduction/01_day_exercises/exercises.js
@@ -0,0 +1,62 @@
+//1. Write a single line comment which says, _comments can make code readable_
+// comments can make code readable
+
+// 2. Write another single comment which says, _Welcome to 30DaysOfJavaScript_
+// Welcome to 30DaysOfJavaScript
+
+
+// 3. Write a multiline comment which says, _comments can make code readable, easy to reuse_
+//    _and informative_
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// variable.js not create, just declare cariables here
+let strVar = 'test';
+let boolVar = true
+let undefindVar;
+let nullVar = null
+
+// 5. Create datatypes.js file and use the JavaScript **_typeof_** operator to check different data types. Check the data type of each variable
+console.log(typeof strVar); // check the string type
+console.log(typeof boolVar); //check the boolean type
+console.log(typeof undefindVar); // check undefined type
+console.log(typeof nullVar); //check null type
+
+
+// 6. Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var_4; 
+
+// 7. Declare four variables with assigned values
+let var_with_value1 = 1;
+let var_with_value2 = 'test';
+let var_with_value3 = true;
+const PI = 3.1415926;
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'tang';
+let lastNmae = 'jikuo';
+let martitalStatus = true;
+const country = 'China';
+let age = 30;
+
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let fn='t', ln='jk', ms=true, cty='China', ag = 30;
+
+
+// 10. Declare two variables _myAge_ and _yourAge_ and assign them initial values and log to the browser console.
+let myAge = 25;
+let yourAge = 30;
+console.log('I am', myAge,' years old.');
+console.log('You are', yourAge, ' years old.');
+// ```sh
+// I am 25 years old.
+// You are 30 years old.
+// ```
